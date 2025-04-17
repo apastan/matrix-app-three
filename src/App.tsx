267c8226073ae40f4405bf11fdcb9c366a6f14fb
apastan <apastan.dev@gmail.com>
2025-04-17 23:32:32 +0300

    feat: add Sonner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { Container } from '@/components/layouts'
 import { AddAssetToPortfolioDialog } from '@/features/portfolio/ui/'
 import { useGetAllAssets24hrQuery } from '@/features/portfolio/api'
-
+import { Toaster } from '@/components/ui/sonner'
 import { Portfolio } from '@/features/portfolio/ui/portfolio.tsx'
-import { InfiniteLoader } from '@/components/ui'
+import { Button, InfiniteLoader } from '@/components/ui'
+import { useTheme } from 'next-themes'
 
 function App() {
   const { isLoading } = useGetAllAssets24hrQuery()
 
+  const { setTheme } = useTheme()
+
   return (
     <>
+      <Toaster />
       <header className={'pt-5 pb-5'}>
         <Container className={'flex justify-between items-center'}>
           <div>Portfolio Overview</div>
 
+          <Button onClick={() => setTheme('dark')}>Change theme</Button>
           <AddAssetToPortfolioDialog />
         </Container>
       </header>
