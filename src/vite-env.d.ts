9057cb95491a2000b40d611579ff44d354b4db3c
apastan <apastan.dev@gmail.com>
2025-04-23 20:13:25 +0300

    refactor: update `base` settings in Vite config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_BINANCE_BASE_API_URL: string
   readonly VITE_BINANCE_WEBSOCKET_STREAM_API_URL: string
+  readonly VITE_BASE_URL: string
 }
 
 interface ImportMeta {
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const base =
+    mode === 'production' ? env.VITE_BASE_URL || '/matrix-app-three' : '/'
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  base: '/matrix-app-three',
+    base,
+  }
 })
